fb7e87324375a65d50e8c66eb8e3940491c79884
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-21 18:34:34 +0000

    update tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,19 +2,35 @@
 
 const supertest = require('supertest');
 const { app } = require('../src/server');
+const { sequelizeDB } = require('../src/auth/models');
 
 const request = supertest(app);
 
-describe('Auth login', () => {
+beforeAll(async () => {
+  await sequelizeDB.sync();
+});
+
+afterAll(async () => {
+  await sequelizeDB.drop();
+});
+
+describe('Auth router', () => {
   it('should create a new user', async () => {
     const response = await request.post('/signup').send({
       username: 'test',
       password: '123456',
     });
 
-    console.log(response.body);
-    expect(response.username).toBe('test');
+    // console.log(response.body);
+    expect(response.body.username).toBe('test');
     // expect(response.status).toBe(403);
   });
 
+  it('should login a user', async () => {
+    const response = await request.post('/signin').auth('test', '123456');
+
+    expect(response.body.username).toBe('test');
+    expect(response.status).toBe(200);
+    expect(response.body.password).toBeTruthy();
+  });
 });
