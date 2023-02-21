0b402275fcf01c5914ad82b6ce990d67eb1d52f0
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-21 01:57:16 +0000

    modularize code

---
+++ src/auth/models/users-model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+
